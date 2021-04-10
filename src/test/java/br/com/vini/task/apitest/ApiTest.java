86f215d3dd812fa1d1b15a546a9f180684aefa13
vinicius-guima <vinicius_guima.social@hotmail.com>
2021-04-10 15:53:40 -0300

    adicionando arquivos de src r pom

---
+++ src/test/java/br/com/vini/task/apitest/ApiTest.java
@@ -0,0 +1,61 @@
+package br.com.vini.task.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class ApiTest {
+	
+	@BeforeClass
+	public static void setUp() {
+		RestAssured.baseURI  = "http://localhost:8888/tasks-backend";
+	}
+
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured
+			.given()
+			.when ()
+				.get("/todo")
+			.then()
+				.statusCode(200)
+				.log().all();
+	}
+	
+	@Test
+	public void deveSalvarTarefas() {
+		RestAssured
+			.given()
+				.body("{ \"task\":\"Teste via api\", \"dueDate\":\"2021-05-04\" }")
+				.contentType(ContentType.JSON)
+			.when ()
+				.post("/todo")
+			.then()
+				.statusCode(201)
+				.log().all();
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefasInvalida() {
+		RestAssured
+			.given()
+				.body("{ \"task\":\"Teste via api\", \"dueDate\":\"2019-05-04\" }")
+				.contentType(ContentType.JSON)
+			.when ()
+				.post("/todo")
+			.then()
+				.statusCode(400)
+				.body("message", CoreMatchers.is("Due date must not be in past"))
+				.log().all();
+	}
+}
+
+
+
+
+
+
+
